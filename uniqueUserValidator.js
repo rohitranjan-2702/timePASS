cf5a3f2e08be496e75472fae0ec85e117fd277d5
ROHIT RANJAN SINGH <68217537+rohitranjan-2702@users.noreply.github.com>
2023-02-15 17:52:50 +0530

    Update uniqueUserValidator.js

--- uniqueUserValidator.js
+++ uniqueUserValidator.js
@@ -18,3 +18,25 @@ if (isExists) {
 } else {
   console.log(`The username '${username}' does not exist in the JSON file.`);
 }
+
+
+// reading form file
+// const fs = require('fs'); 
+
+// // define the function to check if the username exists
+// function isUsernameExists(username, jsonFilePath) {
+//   const data = JSON.parse(fs.readFileSync(jsonFilePath)); 
+//   const usernames = data.map(user => user.username);
+//   return usernames.includes(username); 
+// }
+
+// // example usage:
+// const username = 'john_doe';
+// const jsonFilePath = 'users.json';
+// const isExists = isUsernameExists(username, jsonFilePath);
+
+// if (isExists) {
+//   console.log(`The username '${username}' already exists in the JSON file.`);
+// } else {
+//   console.log(`The username '${username}' does not exist in the JSON file.`);
+// }
